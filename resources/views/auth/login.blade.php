c5a08ca527ebd7f41556e082553cacd0ad16ada5
Amri <amrihakim12@student.uns.ac.id>
2022-06-08 23:34:51 +0700

    edit login ui

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.empty', ['paceTop' => true, 'bodyExtraClass' => 'bg-white'])
 
-@section('title', 'SPMB')
+@section('title', 'Login')
 
 @section('content')
 <!-- begin login -->
@@ -9,9 +9,9 @@
 	<div class="news-feed">
 		<div class="news-image" style="background-image: url(/assets/img/login-bg/front.png)"></div>
 		<div class="news-caption">
-			<h4 class="caption-title"><b>S</b>PMB</h4>
+			<h4 class="caption-title">RT-RW</h4>
 			<p>
-				Sistem Pendaftaran Mahasiswa Baru
+				RT-RW Online
 			</p>
 		</div>
 	</div>
