466e71d025286aeb84cf892f44b3efef450acf91
Amri <amrihakim12@student.uns.ac.id>
2022-07-15 16:32:01 +0700

    add filter date between in presensi ronda

--- resources/views/pages/admin/ronda/presensi/index.blade.php
+++ resources/views/pages/admin/ronda/presensi/index.blade.php
@@ -7,7 +7,9 @@
 <link href="{{ asset('/assets/plugins/datatables.net-bs4/css/dataTables.bootstrap4.css') }}" rel="stylesheet" />
 <link href="{{ asset('/assets/plugins/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css') }}" rel="stylesheet" />
 <link href="{{ asset('/assets/plugins/datatables.net-buttons-bs4/css/buttons.bootstrap4.min.css') }}" rel="stylesheet" />
+
 <!-- end datatables -->
+<link href="{{ asset('/assets/plugins/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css') }}" rel="stylesheet" />
 @endpush
 
 @section('content')
@@ -36,6 +38,34 @@
   <!-- end panel-heading -->
   <!-- begin panel-body -->
   <div class="panel-body">
+    <div class="container">
+        <form action="{{ route('admin.ronda.presensi.index') }}" method="GET">
+        {{-- <input type="d" class="form-control" name="email" value="{{\Request::get('email')}}"> --}}
+        <div class="row my-2">
+            <div class="col-md-2 my-auto">
+                <label for="tanggal_awal"><strong>Tanggal Awal</strong></label>
+            </div>
+            <div class="col-md-3">
+                <div class="input-group date">
+                    <input type="date" id="tanggal_awal" name="tanggal_awal" class="form-control" autofocus data-parsley-required="true" value="{{\Request::get('tanggal_awal')}}">
+                </div>
+            </div>
+            <div class="col-md-2 my-auto">
+                <label for="tanggal_akhir"><strong>Tanggal Akhir</strong></label>
+            </div>
+            <div class="col-md-3">
+                <div class="input-group date">
+                    <input type="date" id="tanggal_akhir" name="tanggal_akhir" class="form-control" autofocus data-parsley-required="true" value="{{\Request::get('tanggal_akhir')}}">
+                </div>
+            </div>
+            <button type="submit" class="btn btn-primary"> Cari </button>
+            {{-- <button type="submit" class="btn btn-primary"> Cari </button> --}}
+            <a href="{{ route('admin.ronda.presensi.index') }}" class="btn btn-white mx-2">Hapus Filter</a>
+        </div>
+        {{-- <button type="reset" class="btn btn-white"> Reset </button> --}}
+        </form>
+      </div>
+      <hr>
     {{ $dataTable->table() }}
   </div>
   <!-- end panel-body -->
@@ -55,4 +85,21 @@
     $('.buttons-reload').trigger('click')
   })
 </script>
+<script src="{{ asset('/assets/plugins/parsleyjs/dist/parsley.js') }}"></script>
+<script src="{{ asset('/assets/plugins/smartwizard/dist/js/jquery.smartWizard.js') }}"></script>
+<script src="{{ asset('/assets/js/demo/form-wizards-validation.demo.js') }}"></script>
+<script src="{{ asset('/assets/plugins/parsleyjs/dist/parsley.js') }}"></script>
+<script src="{{ asset('/assets/js/custom/datetime-picker.js') }}"></script>
+<script src="{{ asset('/assets/js/custom/string-helper.js') }}"></script>
+<script>
+    $( document ).ready(function() {
+        $("#from-datepicker").datepicker({
+            // format: 'yyyy-mm-dd'
+        });
+        $("#from-datepicker").on("change", function () {
+            var fromdate = $(this).val();
+            alert(fromdate);
+        });
+    });
+    </script>
 @endpush
